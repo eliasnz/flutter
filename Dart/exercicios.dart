453253edebe7e6ee998fccb9df33ab982f7dfacb
Elias Nazareno <elias_cross@hotmail.com>
2020-10-19 12:12:23 -0300

    commit att projects

--- Dart/exercicios.dart
+++ Dart/exercicios.dart
@@ -1,3 +1,12 @@
 main() {
-  print('Teste Dart');
+  var nome = 'Elias';
+  var sobrenome = 'Nazareno';
+  print("$nome $sobrenome");
+
+  List<String> compras = ['banana', 'maça'];
+
+  compras.add('123');
+
+  print(compras);
 }
+ 
